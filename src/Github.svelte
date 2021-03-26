370fed81d7d2379f5d467b279d8ee465aa327dca
Nelson Foltz <nelsfoltz@gmail.com>
2021-03-26 16:28:55 -0400

    add: http using useMount

--- src/Github.svelte
+++ src/Github.svelte
@@ -8,7 +8,8 @@
   onMount(async () => {
     let userData = await fetch("https://api.github.com/users");
     let githubUsers = await userData.json();
-    console.log(githubUsers);
+    users = githubUsers;
+    loading = false;
   });
 </script>
 
@@ -21,5 +22,15 @@
 {#if loading}
   <h2>Loading...</h2>
 {:else}
-  <h2>Data</h2>
+  <section>
+    {#each users as user}
+      <article class="user">
+        <img src={user.avatar_url} alt={user.login} />
+        <div class="user-info">
+          <h3>User: {user.login}</h3>
+          <a href={user.html_url} class="btn-primary" target="_blank">github url</a>
+        </div>
+      </article>
+    {/each}
+  </section>
 {/if}
